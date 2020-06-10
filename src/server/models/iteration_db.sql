d571af43e5bd7921d257ea84b96b8a38d37503d2
Nick Healy <nickrhealy@gmail.com>
2020-06-10 14:10:42 -0700

    create queries to get resources with likes, get comments, and edit comments

--- src/server/models/iteration_db.sql
+++ src/server/models/iteration_db.sql
@@ -1,5 +1,4 @@
 Drop table if exists public.resources; 
-Drop table if exists public.techs; 
 Drop table if exists public.comments; 
 Drop table if exists public.likes; 
 Drop table if exists public.users;
@@ -23,6 +22,8 @@ CREATE TABLE public.comments (
   "user_id" bigint NOT NULL,
   "resource_id" bigint NOT NULL,
   "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  "edited" boolean DEFAULT false, 
+  "last_updated" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT "comments_pk" PRIMARY KEY ("_id")
 ) WITH (
   OIDS=FALSE
@@ -41,7 +42,7 @@ CREATE TABLE public.likes (
 
 CREATE TABLE public.users (
   "_id" bigserial NOT NULL,
-  "user_name" varchar NOT NULL,
+  "user_name" varchar UNIQUE NOT NULL,
   "email" varchar NOT NULL,
   "icon" varchar NOT NULL,
   "password" varchar NOT NULL,
@@ -59,40 +60,57 @@ ALTER TABLE public.likes ADD CONSTRAINT "likes_fk1" FOREIGN KEY ("user_id") REFE
 
 -- POPULATING DATA
 
--- POPULATE TECHS TABLE
--- INSERT INTO techs (tech) VALUES ('react');
--- INSERT INTO techs (tech) VALUES ('javascript');
--- INSERT INTO techs (tech) VALUES ('puppeteer');
--- INSERT INTO techs (tech) VALUES ('express');
--- INSERT INTO techs (tech) VALUES ('typescript');
--- INSERT INTO techs (tech) VALUES ('enzyme');
--- INSERT INTO techs (tech) VALUES ('jest');
--- INSERT INTO techs (tech) VALUES ('mongodb');
--- INSERT INTO techs (tech) VALUES ('angular');
--- INSERT INTO techs (tech) VALUES ('redux');
--- INSERT INTO techs (tech) VALUES ('vue');
--- INSERT INTO techs (tech) VALUES ('node');
 
 -- POPULATE RESOURCES TABLE
 
 -- to add a resource: 
 -- INSERT INTO resources (name, url, likes, description, tech) VALUES ('React Official Docs', 'www.IloveReact.com', 50, 'The docs for React', 'react');
 
+-- =============  COMMENTS QUERIES =============
+-- to delete a comment: 
+-- DELETE FROM comments WHERE _id = someId; 
+
 -- to add a comment:
 -- INSERT INTO comments (body, user_id, resource_id) VALUES ('I am Matt!', 1, 1); 
 
+-- GET REQUEST FOR COMMENTS -- JOIN COMMENTS WITH USERS INFORMATION
+-- SELECT comments.*, users.user_name, users.icon FROM comments JOIN users ON comments.user_id = users._id WHERE comments.resource_id = 2;
+
+-- STRETCH: update comments
+-- UPDATE comments SET body = 'New Comment', last_updated = CURRENT_TIMESTAMP, edited = true  WHERE _id = 3; 
+
 --  ======== LIKE QUERIES ================
 
--- to add a like/dislike from neutral state, or change to like/dislike from disliked state 
--- INSERT INTO likes (liked, resource_id, user_id) 
--- VALUES ($1, 1, 1)
--- ON CONFLICT (resource_id, user_id) 
--- DO UPDATE SET liked = $1;
+-- WANT TO NEUTRAL TO LIKED, AND SEE OVERALL LIKE COUNT GO UP BY ONE
+-- TRANSACTION ("Neutral to Liked")
+-- BEGIN;
+-- INSERT INTO likes (liked, resource_id, user_id) VALUES (true, 3, 3);
+-- UPDATE resources SET likes = likes + 1 WHERE _id = 3;
+-- COMMIT; 
 
--- db.query(query, ([true]))
+-- WANT TO GO FROM LIKED TO NEUTRAL, AND SEE OVERALL LIKE COUNT GO DOWN BY ONE
+-- TRANSACTION ("Neutral to Liked")
+-- BEGIN;
+-- DELETE FROM likes WHERE resource_id = 3 AND user_id = 3;
+-- UPDATE resources SET likes = likes - 1 WHERE _id = 3;
+-- COMMIT; 
 
+-- WANT TO GO FROM NEUTRAL TO DISLIKED, AND SEE OVERALL LIKE COUNT GO DOWN BY ONE
+-- BEGIN;
+-- INSERT INTO likes (liked, resource_id, user_id) VALUES (false, 3, 3);
+-- UPDATE resources SET likes = likes - 1 WHERE _id = 3;
+-- COMMIT; 
 
--- to go back to neutral from either like or dislike
--- DELETE FROM likes WHERE resource_id = 1 AND user_id = 1; 
+-- WANT TO GO FROM DISLIKED TO NEUTRAL, AND SEE OVERALL LIKE COUNT GO UP BY ONE
+-- BEGIN;
+-- DELETE FROM likes WHERE resource_id = 3 AND user_id = 3;
+-- UPDATE resources SET likes = likes + 1 WHERE _id = 3;
+-- COMMIT; 
 
 -- Depending on whether user has liked or disliked a resource, the front end must send either one of these two queries 
+
+-- ================ JOINS/delivering GET request ===============
+
+-- to get ALL resources of a particular topic, ordered by likes (descending), and with user-specific info on what they like, dislike, or are neutral about
+-- SELECT resources.*, likes.liked FROM resources  LEFT JOIN likes ON resources._id = likes.resource_id AND likes.user_id = 3 WHERE resources.tech = 'javascript' ORDER BY likes DESC;
+

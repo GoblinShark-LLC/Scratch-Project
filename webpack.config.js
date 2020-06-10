33bb3bb6ec21ae16de5d8c989d3141eb668b1ee3
andrewjcho84 <andrewjcho84@gmail.com>
2020-06-10 10:31:24 -0700

    added proxy to webpack for user

--- webpack.config.js
+++ webpack.config.js
@@ -5,42 +5,40 @@ module.exports = {
   entry: path.resolve(__dirname, './src/client/index.js'),
   output: {
     path: path.resolve(__dirname, './build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, 'src/client'),
     proxy: {
-      '/resource' : 'http://localhost:3000',
-      hot: true
-    }
+      '/resource': 'http://localhost:3000',
+      '/user': 'http://localhost:3000',
+      hot: true,
+    },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }, 
+        use: ['style-loader', 'css-loader'],
+      },
       {
         test: /\.(js|jsx)$/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/client/index.html'
-    })
+      template: './src/client/index.html',
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
-}
+    extensions: ['.js', '.jsx'],
+  },
+};

f8ee4dd636493c984e8755c66981a9dc92fc13d5
Nick Healy <nickrhealy@gmail.com>
2020-06-10 18:06:38 -0700

    complete commentsController.deleteComment

--- src/server/controllers/commentsController.js
+++ src/server/controllers/commentsController.js
@@ -30,19 +30,52 @@ commentsController.getComments = (req, res, next) => {
     })
 };
 
-commentsController.addComments = (req, res, next) => {
+commentsController.addComment = (req, res, next) => {
   // comment body is in the request body
   const { body } = req.body; 
+
+  // id of commenting user, and id or resource on which they are commenting is in req.params
   const { userId, resourceId } = req.params;
 
   const query = `INSERT INTO comments (body, user_id, resource_id) 
                  VALUES ($1, $2, $3)
-                 RETURNING _id;`
+                 RETURNING *;`
   
   const values = [body, userId, resourceId];
 
   db.query(query, values)
-    .then()
-}
+    .then(id => {
+      // send back id of newly created comment
+      res.locals.commentId = id.rows[0]._id;
+      return next();
+    })
+    .catch(err => {
+      return next({
+        log: 'Error in commentsController.addComment',
+        message: { err: `ERROR in commentsController.addComment ${err}` },
+      })
+    })
+};
+
+commentsController.deleteComment = (req, res, next) => {
+  // id of desired comment in req.params
+  const { commentId } = req.params; 
+
+  const query = `DELETE FROM comments 
+                 WHERE _id = $1; `
+  
+  const value = [ commentId ]; 
+
+  db.query(query, value)
+    .then(() => next())
+    .catch(err => {
+      return next({
+        log: 'Error in commentsController.deleteComment',
+        message: { err: `ERROR in commentsController.deleteComment ${err}` },
+      })
+    })
+};
+
+
 
 module.exports = commentsController; 
